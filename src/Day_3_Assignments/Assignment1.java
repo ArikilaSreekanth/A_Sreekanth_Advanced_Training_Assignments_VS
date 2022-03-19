16380c53f0c81162cea5272ed5f155d59b9e15da
ArikilaSreekanth <97657627+ArikilaSreekanth@users.noreply.github.com>
2022-03-19 13:40:08 +0530

    Add files via upload

---
+++ src/Day_3_Assignments/Assignment1.java
@@ -0,0 +1,26 @@
+package Day_3_Assignments;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.fpt.nhatnk.pms.ui.ProductConsole;
+
+import Assignment6_2.Product;
+
+/**
+ *
+ * @author Nhat
+ */
+public class ProductManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ProductConsole pc = new Product();
+        pc.start();
+    }
+    
+}
